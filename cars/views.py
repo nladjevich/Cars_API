36065c9f8a4b180c5a90ce62d8d14b97ea7eaa64
Nicholas Ladjevich <zantladjevich@gmail.com>
2022-10-08 21:47:09 -0400

    Created new view function for getting cars by id + added new urls path for car_detail view function

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,8 @@
 from pickle import GET
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+import cars
 from .serializers import CarSerializer
 from .models import Car
 from rest_framework import status
@@ -21,3 +23,16 @@ def cars_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def car_detail(request, pk):
+
+    print(pk)
+    try:
+        car = Car.objects.get(pk=pk)
+        serializer = CarSerializer(car)
+        return Response(serializer.data)
+    except Car.DoesNotExist:
+        return Response(satus=status.HTTPS_404_NOT_FOUND)
+
+
